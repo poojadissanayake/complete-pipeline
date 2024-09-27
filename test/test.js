50f4dab139b134bb57a9ee1aa013c8f91817165e
Pooja Dissanayake <poojadissanayake6@gmail.com>
2024-09-27 14:50:25 +1000

    test

--- test/test.js
+++ test/test.js
@@ -23,8 +23,8 @@ describe("Application Deployment Tests", function () {
     // Test for feedback API status code
     it("should submit feedback and return success message", function (done) {
         const feedbackData = {
-            name: "Testuser29",
-            email: "tester29@example.com",
+            name: "Testuser30",
+            email: "tester30@example.com",
             review: "Great service!"
         };
 
