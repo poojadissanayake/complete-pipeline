79f851d1aa5230a831e6cfc350dadec11e8e7479
Pooja Dissanayake <poojadissanayake6@gmail.com>
2024-09-27 15:58:27 +1000

    test

--- test/test.js
+++ test/test.js
@@ -24,7 +24,7 @@ describe("Application Deployment Tests", function () {
     it("should submit feedback and return success message", function (done) {
         const feedbackData = {
             name: "TestUser",
-            email: "tester34@example.com",
+            email: "tester35@example.com",
             review: "Great service!"
         };
 
